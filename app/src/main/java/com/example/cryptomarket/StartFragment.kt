12db62bc2cb0bc870856d8ba3c32be9a3dac8044
Ed Cres <crespoed07@gmail.com>
2022-06-29 23:29:07 -0400

    Made a shared viewModel

--- app/src/main/java/com/example/cryptomarket/StartFragment.kt
+++ app/src/main/java/com/example/cryptomarket/StartFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
+private const val fragmentTAG = "StartFrag__TAG"
+
 class StartFragment : Fragment() {
 
     override fun onCreateView(
---
+++ app/src/main/java/com/example/cryptomarket/coinsViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.cryptomarket
+
+import androidx.lifecycle.ViewModel
+
+private const val TAG = "CoinsVM__TAG"
+
+class coinsViewModel: ViewModel() {
+}
