57bcb3e0d9f0dd8c6105c67e7e40401eecaf9d6f
Ed Cres <crespoed07@gmail.com>
2022-06-29 23:19:23 -0400

    Created a navigation file and the start fragment.

--- app/src/main/java/com/example/cryptomarket/MainActivity.kt
+++ app/src/main/java/com/example/cryptomarket/MainActivity.kt
@@ -3,6 +3,24 @@ package com.example.cryptomarket
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/** todo:
+ *
+ * - jetpack navigation
+ * - viewModel
+ * - project file structure
+ * - data/backend stuff
+ * - dataBinding
+ * - dependencies
+ * - API service
+ * -
+ *
+ */
+
+/** maybe:
+ *
+ * - local database
+ */
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
