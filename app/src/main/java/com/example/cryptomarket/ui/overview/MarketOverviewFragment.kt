dc592349ed640e1fec8b18ae8ef0753b1d0750f8
Ed Cres <crespoed07@gmail.com>
2022-07-02 06:44:19 -0400

    Set up databinding and the basic UI for market overview.

--- app/src/main/java/com/example/cryptomarket/ui/overview/MarketOverviewFragment.kt
+++ app/src/main/java/com/example/cryptomarket/ui/overview/MarketOverviewFragment.kt
@@ -6,13 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.cryptomarket.R
+import com.example.cryptomarket.databinding.FragmentMarketOverviewBinding
+import com.example.cryptomarket.databinding.FragmentNewsBinding
 
 class MarketOverviewFragment : Fragment() {
+
+    private var binding: FragmentMarketOverviewBinding? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_market_overview, container, false)
+        val fragmentBinding = FragmentMarketOverviewBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
     }
 }
