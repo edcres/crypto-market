576f7a1300b16bb483a412b19713957ca0fc429a
Ed Cres <crespoed07@gmail.com>
2022-07-02 06:21:17 -0400

    Set up the foundation for the coins list adapter.

--- app/src/main/java/com/example/cryptomarket/ui/coins/CoinsListAdapter.kt
+++ app/src/main/java/com/example/cryptomarket/ui/coins/CoinsListAdapter.kt
@@ -1,29 +1,42 @@
 package com.example.cryptomarket.ui.coins
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cryptomarket.data.DummyDataClass
+import com.example.cryptomarket.databinding.DummyRecyclerItemBinding
 
 class CoinsListAdapter()
     : ListAdapter<DummyDataClass, CoinsListAdapter.CoinsViewHolder>(CoinsDiffCallback()) {
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): CoinsListAdapter.CoinsViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoinsViewHolder {
+        return CoinsViewHolder.from(parent)
     }
 
-    override fun onBindViewHolder(holder: CoinsListAdapter.CoinsViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
+    override fun onBindViewHolder(holder: CoinsViewHolder, position: Int) =
+        holder.bind(getItem(position))
 
     class CoinsViewHolder private constructor(
-        val binding: 
-    ): RecyclerView.ViewHolder {
+        private val binding: DummyRecyclerItemBinding
+    ): RecyclerView.ViewHolder(binding.root) {
 
+        fun bind(dummyDataClass: DummyDataClass) {
+            binding.apply {
+                // todo:
+                executePendingBindings()
+            }
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): CoinsViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = DummyRecyclerItemBinding
+                    .inflate(layoutInflater, parent, false)
+                return CoinsViewHolder(binding)
+            }
+        }
     }
 
     class CoinsDiffCallback : DiffUtil.ItemCallback<DummyDataClass>() {
