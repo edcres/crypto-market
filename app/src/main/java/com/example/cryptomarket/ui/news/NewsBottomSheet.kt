47a2d5730009fd4b3d30e4f75485e1a082773afd
Ed Cres <crespoed07@gmail.com>
2022-07-03 17:52:51 -0400

    Made the bottom sheet layout and fragment for news.

--- app/src/main/java/com/example/cryptomarket/ui/news/NewsBottomSheet.kt
+++ app/src/main/java/com/example/cryptomarket/ui/news/NewsBottomSheet.kt
@@ -1,19 +1,47 @@
 package com.example.cryptomarket.ui.news
 
+import android.app.Dialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
+import com.example.cryptomarket.R
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+private const val NEWS_SHEET_STR_KEY = "Coins companion tag"
 
 class NewsBottomSheet : BottomSheetDialogFragment() {
 
     private lateinit var sheetTxt: TextView
     private lateinit var dialog: BottomSheetDialog
 
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        dialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
+        return dialog
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.news_bottom_sheet, container, false)
+        sheetTxt = view.findViewById(R.id.news_sheet_txt)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.getString(NEWS_SHEET_STR_KEY)
+    }
+
     companion object {
         const val TAG = "NewsSheet_TAG"
-        fun newInstance(testString: String) = ModalBottomSheet().apply {
+        fun newInstance(testString: String) = NewsBottomSheet().apply {
             arguments = Bundle().apply {
-                putString(SHEET_STR_KEY, testString)
+                putString(NEWS_SHEET_STR_KEY, testString)
             }
         }
     }
