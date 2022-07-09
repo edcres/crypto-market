fcc1391f91055ba3cf48601b9363bb1fe96583b9
Ed Cres <crespoed07@gmail.com>
2022-07-08 23:07:02 -0400

    Removed the dummy data .xml and the data class.

--- app/src/main/java/com/example/cryptomarket/data/DummyDataClass.kt
+++
@@ -1,6 +0,0 @@
-package com.example.cryptomarket.data
-
-// todo: replace this
-data class DummyDataClass (
-    val data: String = "dummy"
-)
--- app/src/main/java/com/example/cryptomarket/ui/coins/CoinsListAdapter.kt
+++ app/src/main/java/com/example/cryptomarket/ui/coins/CoinsListAdapter.kt
@@ -5,7 +5,6 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.example.cryptomarket.data.DummyDataClass
 import com.example.cryptomarket.data.coinsapi.ticker.Ticker
 import com.example.cryptomarket.databinding.CoinChartRecyclerItemBinding
 
