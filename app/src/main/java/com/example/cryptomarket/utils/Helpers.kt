72e1b2173051fa3f90ed097cef758f13008abc03
Ed Cres <crespoed07@gmail.com>
2022-07-09 01:03:24 -0400

    Optimized reformatting data string and made it m/d/y.

--- app/src/main/java/com/example/cryptomarket/utils/Helpers.kt
+++ app/src/main/java/com/example/cryptomarket/utils/Helpers.kt
@@ -23,9 +23,12 @@ fun pickPercentChange(timeFrame: DateFrame, priceData: PriceData) = when(timeFra
         DateFrame.YEAR -> priceData.percent_change_1y
 }
 
-// 2022-07-08T23:27:51Z
 fun formatNewsPostDate(rawDateString: String): String {
-    val dateList = rawDateString.split("-")
-    val dayOfMonth = "${dateList[2][0]}${dateList[2][1]}"
-    return "${dateList[0]}/${dateList[1]}/$dayOfMonth"
+    // raw date = 2022-07-08T23:27:51Z
+    // raw date -> y/m/d -> m/d/y
+    // raw date -> 2022/07/08 -> 07/08/2022
+    val listYMD = rawDateString.split("-").map {
+        if (it.contains(":")) "${it[0]}${it[1]}" else it
+    }
+    return "${listYMD[1]}/${listYMD[2]}/${listYMD[0]}"
 }
