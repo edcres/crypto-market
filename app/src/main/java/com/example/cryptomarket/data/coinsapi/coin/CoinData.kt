ee64a1f91a94267e242ffbcf45aefc20fdc946ed
Ed Cres <crespoed07@gmail.com>
2022-07-12 23:40:56 -0400

    Made other attributes nullable in the CoinData data class.

--- app/src/main/java/com/example/cryptomarket/data/coinsapi/coin/CoinData.kt
+++ app/src/main/java/com/example/cryptomarket/data/coinsapi/coin/CoinData.kt
@@ -14,23 +14,23 @@ data class CoinData (
     @Json(name = "rank")
     val rank: Int,
     @Json(name = "is_new")
-    val isNew: Boolean,
+    val isNew: Boolean?,
     @Json(name = "is_active")
-    val isActive: Boolean,
+    val isActive: Boolean?,
     @Json(name = "type")
-    val type: String,
+    val type: String?,
     @Json(name = "team")
-    val team: List<TeamMember>,
+    val team: List<TeamMember>?,
     @Json(name = "description")
-    val description: String,
+    val description: String?,
     @Json(name = "open_source")
-    val openSource: Boolean,
+    val openSource: Boolean?,
     @Json(name = "started_at")
     val startedAt: String?,
     @Json(name = "proof_type")
-    val proofType: String,
+    val proofType: String?,
     @Json(name = "org_structure")
-    val orgStructure: String,
+    val orgStructure: String?,
     @Json(name = "hash_algorithm")
-    val hashAlgorithm: String
+    val hashAlgorithm: String?
 )
