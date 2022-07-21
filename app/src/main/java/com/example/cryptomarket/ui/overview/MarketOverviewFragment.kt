4786932f8feac2affc0400bac4df3615b3b9df60
Ed Cres <crespoed07@gmail.com>
2022-07-21 10:56:21 -0400

    Added the base for the rest of the set up for the pie charts.

--- app/src/main/java/com/example/cryptomarket/ui/overview/MarketOverviewFragment.kt
+++ app/src/main/java/com/example/cryptomarket/ui/overview/MarketOverviewFragment.kt
@@ -1,7 +1,12 @@
 package com.example.cryptomarket.ui.overview
 
 import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.text.style.RelativeSizeSpan
+import android.text.style.StyleSpan
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -18,8 +23,14 @@ import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
 import com.github.mikephil.charting.highlight.Highlight
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener
+import com.github.mikephil.charting.utils.ColorTemplate
+import com.github.mikephil.charting.utils.MPPointF
 
 private const val TAG = "MarketListFrag__TAG"
 
@@ -102,7 +113,7 @@ class MarketOverviewFragment : Fragment() {
                 pieChart.description.isEnabled = true
                 pieChart.setExtraOffsets(5f, 10f, 5f, 5f)
                 pieChart.dragDecelerationFrictionCoef = 0.95f
-//            pieChart.centerText = generateCenterSpannableText()
+                pieChart.centerText = generateCenterSpannableText()
 
                 pieChart.isDrawHoleEnabled = true
                 pieChart.setHoleColor(Color.WHITE)
@@ -130,13 +141,65 @@ class MarketOverviewFragment : Fragment() {
                 pieChart.setEntryLabelColor(Color.WHITE)
                 pieChart.setEntryLabelTextSize(12f)
 
-                setPiesData(pieChart, 6, 14f)
+                // todo: set the items and the number of items for that pie
+                setPiesData(pieChart, 1, 14f)
             }
         }
     }
 
+    // todo: edit all of this class, and maybe don't even pass the values as parameters
     private fun setPiesData(pieChart: PieChart, count: Int, range: Float) {
-        
+        val entries = ArrayList<PieEntry>()
+        // todo: edit the pie entries to the crypto data
+        for (i in 0 until count) {
+            entries.add(
+                PieEntry(
+                    (Math.random() * range + range / 5).toFloat(),
+                    parties[i % parties.size],
+                    resources.getDrawable(R.drawable.star)
+                )
+            )
+        }
+
+        // todo: change the data set to the crypto data
+        val dataSet = PieDataSet(entries, "Election Results")
+        dataSet.setDrawIcons(false)
+        dataSet.sliceSpace = 3f
+        dataSet.iconsOffset = MPPointF(0f, 40f)
+        dataSet.selectionShift = 5f
+        // todo: add custom colors
+        val colors = java.util.ArrayList<Int>()
+        for (c in ColorTemplate.VORDIPLOM_COLORS) colors.add(c)
+        for (c in ColorTemplate.JOYFUL_COLORS) colors.add(c)
+        for (c in ColorTemplate.COLORFUL_COLORS) colors.add(c)
+        for (c in ColorTemplate.LIBERTY_COLORS) colors.add(c)
+        for (c in ColorTemplate.PASTEL_COLORS) colors.add(c)
+        colors.add(ColorTemplate.getHoloBlue())
+        dataSet.colors = colors
+
+        val data = PieData(dataSet)
+        data.setValueFormatter(PercentFormatter())
+        data.setValueTextSize(11f)
+        data.setValueTextColor(Color.WHITE)
+        //        data.setValueTypeface(tfLight)
+        pieChart.data = data
+
+        // undo all highlights
+        pieChart.highlightValues(null)
+        pieChart.invalidate()
+    }
+
+    // for pie chart
+    // todo: change what this says
+    private fun generateCenterSpannableText(): SpannableString? {
+        val s = SpannableString("MPAndroidChart\ndeveloped by Philipp Jahoda")
+        s.setSpan(RelativeSizeSpan(1.7f), 0, 14, 0)
+        s.setSpan(StyleSpan(Typeface.NORMAL), 14, s.length - 15, 0)
+        s.setSpan(ForegroundColorSpan(Color.GRAY), 14, s.length - 15, 0)
+        s.setSpan(RelativeSizeSpan(.8f), 14, s.length - 15, 0)
+        s.setSpan(StyleSpan(Typeface.ITALIC), s.length - 14, s.length, 0)
+        s.setSpan(ForegroundColorSpan(ColorTemplate.getHoloBlue()), s.length - 14, s.length, 0)
+        return s
     }
     // MAKE CHARTS //
 }
