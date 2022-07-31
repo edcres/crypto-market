784889ba380827ce08cbc5ebfc36c0f87d6ccf2b
Ed Cres <crespoed07@gmail.com>
2022-07-31 00:34:29 -0400

    Implemented a back pressed listener for the webview.

--- app/src/main/java/com/example/cryptomarket/ui/news/NewsFragment.kt
+++ app/src/main/java/com/example/cryptomarket/ui/news/NewsFragment.kt
@@ -6,9 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.core.view.doOnDetach
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -16,6 +14,7 @@ import com.example.cryptomarket.R
 import com.example.cryptomarket.databinding.FragmentNewsBinding
 import com.example.cryptomarket.ui.CryptoViewModel
 import com.example.cryptomarket.utils.FragChosen
+import androidx.activity.addCallback
 
 private const val TAG = "NewsListFrag__TAG"
 
@@ -41,15 +40,13 @@ class NewsFragment : Fragment() {
             lifecycleOwner = viewLifecycleOwner
             newsListRecycler.adapter = newsListAdapter
             newsListRecycler.layoutManager = LinearLayoutManager(requireContext())
-//            newsWebView.doOnDetach {  }   todo:
             newsWebView.settings.javaScriptEnabled = true
+            requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+                newsWebView.goBack()
+            }
             closeWebBtn.setOnClickListener { closeWebView() }
-
         }
         setObservers()
-
-
-//        requireActivity().onBackPressed()
     }
 
     override fun onDestroy() {
