03d6ba2a59611013c7a012ef31b9b06bea04cc5a
Ed Cres <crespoed07@gmail.com>
2022-08-01 19:52:45 -0400

    Added some comments explaining the app.

--- app/src/main/java/com/example/cryptomarket/ui/MainActivity.kt
+++ app/src/main/java/com/example/cryptomarket/ui/MainActivity.kt
@@ -8,7 +8,23 @@ import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.ContextCompat
 import com.example.cryptomarket.R
 
-
+/** App explanation:
+ *
+ * The app has 3 main navigation destinations
+ * - Coins List
+ *      - Displays a list of crypto coins with a simple graph
+ *      - A bottom expandable sheet with a specific coin
+ *      - The user clicks on a coin and the bottom sheet expands
+ *          - Expanded, the sheet displays more information about the coin
+ *              - Including a detailed interactive line chart
+ * - Market Overview
+ *      - Displays broad information about the overall cryptocurrency market
+ *          - Along with Pie charts comparing some of the top 10 coins
+ * - News List
+ *      - Displays a list of news about cryptocurrency
+ *          - each is clickable and displays a Review and the content of
+ *              a link to the news article.
+ */
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
